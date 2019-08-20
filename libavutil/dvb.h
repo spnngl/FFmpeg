7055d68a612de540f9bc2d02f2280b407177aaf8
Anthony Delannoy <anthony.2lannoy@gmail.com>
2019-08-20 18:15:45 +0200

    add license to my files

--- libavutil/dvb.h
+++ libavutil/dvb.h
@@ -1,3 +1,28 @@
+/*
+ * copyright (c) 2019 Anthony Delannoy
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file
+ * Public header for DVB Sections.
+ */
+
 #ifndef AVUTIL_DVB_H
 #define AVUTIL_DVB_H
 
--- libavutil/smoothing_buffer_tables.h
+++ libavutil/smoothing_buffer_tables.h
@@ -1,3 +1,29 @@
+/*
+ * epg data decoder - tables used by epg decoder
+ * Copyright (c) 2019 Anthony Delannoy
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file
+ * epg data decoder - tables used by epg decoder
+ */
+
 #ifndef AVUTIL_SMOOTHING_BUFFER_TABLES_H
 #define AVUTIL_SMOOTHING_BUFFER_TABLES_H
 
